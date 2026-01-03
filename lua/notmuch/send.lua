8d510adeba03020d90b3450c8abcf59c963d88c9
Yousef Akbar <yhakbar99@gmail.com>
2026-01-03 23:57:14 +0300

    fix(send): use cross-platform tempfile for compose
    Replace mktemp with vim.fn.tempname() to support Windows and all
    platforms where mktemp is not available

--- lua/notmuch/send.lua
+++ lua/notmuch/send.lua
@@ -292,15 +292,7 @@ end
 --   require('notmuch.send').compose()
 s.compose = function(to)
   to = to or ''
-	local mktemp = vim.system({ 'mktemp', '-t', 'compose.XXXXXX.eml' }):wait()
-	if mktemp.code ~= 0 then
-		vim.notify(
-			'❌ Failed to create temporary file \n' .. mktemp.stderr,
-			vim.log.levels.ERROR
-		)
-		return
-	end
-	local compose_filename = vim.trim(mktemp.stdout)
+  local compose_filename = vim.fn.tempname() .. '-compose.eml'
 
   -- TODO: Add ability to modify default body message and signature
   local headers = {
